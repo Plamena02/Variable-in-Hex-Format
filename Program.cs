b1f59f4d02522506ce4d3b7d33f56514b7224472
Plamena Nickolova <47273192+Plamena02@users.noreply.github.com>
2019-04-12 16:05:01 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шеснайсеттична_променлива
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = Console.ReadLine();
+            int b = Convert.ToInt32(a, 16);
+            Console.WriteLine(b);
+
+
+        }
+    }
+}
